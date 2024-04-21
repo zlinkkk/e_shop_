25dc31167b9de6c886f07a6524647abea0a30387
temp <yaroslavsevryukov77@gmail.com>
2024-04-21 13:17:42 +0300

    ?

--- e_shop/users/models.py
+++ e_shop/users/models.py
@@ -4,4 +4,4 @@ from django.db import models
 
 class User(models.Model):
     login = models.EmailField(max_length=100, blank=False, null=False)
-    password = models.CharField(max_length=100, null=False, blank=False)
+    # password = models.CharField(max_length=100, null=False, blank=False)
