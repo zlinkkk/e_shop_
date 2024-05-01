1b8d15ea347c0275ef9d9881cb7a249521293dcf
Ярослав <yaroslavsevryukov77@gmail.com>
2024-05-01 17:05:26 +0300

    .

--- e_shop/users/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from django.contrib.auth.forms import AuthenticationForm
-from users.models import User
-
-class LoginForm(AuthenticationForm):
-    class Meta:
-        model = User
-    
--- e_shop/users/models.py
+++ e_shop/users/models.py
@@ -1,7 +1,2 @@
 from django.db import models
 
-# Create your models here.
-
-class User(models.Model):
-    login = models.EmailField(max_length=100, blank=False, null=False)
-    # password = models.CharField(max_length=100, null=False, blank=False)
