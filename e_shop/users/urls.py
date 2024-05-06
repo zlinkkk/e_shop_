d0e4dd6b3b5f63fc59ccaf361781ee26b24bd6dd
Ярослав <yaroslavsevryukov77@gmail.com>
2024-05-06 17:59:26 +0300

    added registration

--- e_shop/users/urls.py
+++ e_shop/users/urls.py
@@ -4,5 +4,8 @@ from users import views
 app_name = 'users'
 
 urlpatterns = [
-    path('login/', views.login_page, name = 'login')
+    path('login/', views.login_page, name = 'login'),
+    path('logout/', views.logout_page, name = 'logout'),
+    path('registration/', views.registration, name = 'register'),
+    path('profile_page/', views.profile, name = 'user_page'),
 ]
--- e_shop/users/views.py
+++ e_shop/users/views.py
@@ -2,10 +2,11 @@ from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 from users.forms import LoginForm
 
 def login_page(request):
-    error_msg = ''
+    # error_msg = ''
     if request.method == 'POST':
         form = LoginForm(request.POST)
         if form.is_valid():
@@ -27,11 +28,22 @@ def login_page(request):
         # 'error_msg': error_msg 
     })
 
+def registration(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = UserCreationForm()
+    return render(request, 'users/register.html', {'form': form})
+
+
+def logout_page(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('profile:login'))
+
 def profile(request):
     return render(request, 'users/profile.html', {
         'title': 'Ваш профиль'
     })
 
-def logout_page(request):
-    logout(request)
-    return HttpResponseRedirect(reverse('profile:login'))
